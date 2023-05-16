d168c1ce31faaf4f137efdbadbc07c4ec6609571
varanustoken <varanustoken@gmail.com>
2023-05-17 00:52:06 +0700

    a

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,12 +12,12 @@
                         <h1 class="text-white iq-tw-7 iq-mb-20"><b class="iq-font-yellow">PIG CHAIN</b> Advanced Technology</h1>
                         <p>Pig Chain use the latest and advanced technology to maintain the scalability of the number of existing transactions </p>
                         <a href="https://t.me/PigChainPortal" class="button iq-mt-20 iq-ml-10" style="background:#DD00AE">JOIN US</a>
-                        <a href="https://poocoin.app/tokens/0xCbF7F81F28c1b3b7110C5DA9d7DC27F14a12Dcfd" class="button bt-white iq-mt-20 iq-ml-10" style="background:#DD00AE;color:white">CHART</a>
-                        <a href="https://pancakeswap.finance/swap?outputCurrency=0xCbF7F81F28c1b3b7110C5DA9d7DC27F14a12Dcfd" class="button bt-white iq-mt-20 iq-ml-10" style="background:#DD00AE;color:white">BUY PIG</a>
+                        <a href="https://poocoin.app/tokens/0X" class="button bt-white iq-mt-20 iq-ml-10" style="background:#DD00AE;color:white">CHART</a>
+                        <a href="https://pancakeswap.finance/swap?outputCurrency=0X" class="button bt-white iq-mt-20 iq-ml-10" style="background:#DD00AE;color:white">BUY PIG</a>
 
                         <div class="row">&nbsp;</div>
 
-                        Contract Address : <b>0xCbF7F81F28c1b3b7110C5DA9d7DC27F14a12Dcfd</b>
+                        Contract Address : <b>0X</b>
                     </div>
                 </div>
                 <div class="col-lg-6 mt-5 mt-lg-0">
